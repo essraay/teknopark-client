e5847c337b95b73f9d7c08f854dcadcf405746b2
essraay <esra-1-@hotmail.com>
2023-03-06 10:54:28 +0300

    dinamic slider

--- src/pages/HomePage/Slider.jsx
+++ src/pages/HomePage/Slider.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react"
 import {
   Autoplay,
   EffectCards,
@@ -10,12 +10,14 @@ import {
   Navigation,
   Pagination,
   Parallax,
-} from "swiper";
-import { Swiper, SwiperSlide } from "swiper/react";
+} from "swiper"
+import { SliderService } from "../../services"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { BASE_IMG } from "../../constants"
 
 const SwiperItem = ({ children }) => (
   <div
-    className=" w-100"
+    className="w-100"
     style={{
       aspectRatio: "2.78",
       backgroundImage: "url('/slider1.png')",
@@ -25,27 +27,28 @@ const SwiperItem = ({ children }) => (
   >
     {children}
   </div>
-);
+)
 
 const SwiperImage = ({ src }) => (
   <SwiperItem>
     <img src={src} className="w-100 h-100" style={{ objectFit: "cover" }} />
   </SwiperItem>
-);
+)
 /* 
 EffectFade,EffectCube,EffectFlip,EffectCoverflow,EffectCards,EffectCreative
 */
 
 const Slider = () => {
-  const [imageList, setImageList] = useState([
-    "/slider/paneltekno.jpg",
-    "/slider/teknoslider2.jpg",
-    "/slider/duyuru3.jpg",
-    "/slider/proje-pazari.jpg",
-    "/slider/teknofest.jpg",
-    "/slider/duyuru.jpg",
-    "/slider/duyuru7.png",
-  ]);
+  const [imageList, setImageList] = useState(false)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    SliderService.getAll().then((res) => {
+      setImageList(res.data)
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <Swiper
       modules={[
@@ -70,20 +73,21 @@ const Slider = () => {
       }}
       pagination={{ clickable: true }}
     >
-      {imageList.map((image) => (
-        <SwiperSlide>
-          <SwiperItem>
-            <div className="position-relative overflow-hidden w-100 mw-100 h-100 mh-100 p-5">
-              <img
-                className="h-100 w-100"
-                style={{ objectFit: "contain" }}
-                src={image}
-              />
-            </div>
-          </SwiperItem>
-        </SwiperSlide>
-      ))}
+      {imageList &&
+        imageList.reverse().map((image) => (
+          <SwiperSlide>
+            <SwiperItem>
+              <div className="position-relative overflow-hidden w-100 mw-100 h-100 mh-100 p-5">
+                <img
+                  className="h-100 w-100"
+                  style={{ objectFit: "contain" }}
+                  src={BASE_IMG + image.resim_Dizin}
+                />
+              </div>
+            </SwiperItem>
+          </SwiperSlide>
+        ))}
     </Swiper>
-  );
-};
-export default Slider;
+  )
+}
+export default Slider
