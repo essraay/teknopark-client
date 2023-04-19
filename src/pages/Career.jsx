b8a41a16d0c1832d06aa10b3e659b235f79c2d03
essraay <esra-1-@hotmail.com>
2023-04-19 10:36:51 +0300

    dinamik kariyer

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,19 +1,12 @@
-import {
-  CButton,
-  CCard,
-  CCardBody,
-  CCardHeader,
-  CCardText,
-  CCardTitle,
-  CContainer,
-} from "@coreui/react"
+import { CCard, CCardBody, CContainer } from "@coreui/react"
 import { useEffect, useState } from "react"
 import { AdvertService } from "../services"
-import LoadingSpinner from "../components/LoadingSpinner"
+import { dateFormat } from "../utils"
+import { BASE_IMG } from "../constants"
 
 const Career = () => {
   const [loading, setLoading] = useState(true)
-  const [items, setItems] = useState(false)
+  const [item, setItems] = useState(false)
 
   useEffect(() => {
     AdvertService.getAll().then((res) => {
@@ -31,23 +24,71 @@ const Career = () => {
       </div>
       <div className="col-12">
         {loading ? (
-          <LoadingSpinner />
+          <section
+            style={{
+              margin: "10% 0",
+            }}
+          >
+            <p className="fw-bolder text-center pb-5 h-auto">
+              Şu anda aktif ilan bulunmamaktadır.
+            </p>
+          </section>
         ) : (
           <div>
-            {items.map((item) => (
-              <CCard className="card-animation tp-shadow3" key={item.id} style={{ margin: "2% 0" }}>
-                <p className="text-center strong">
-                  {item.pozisyon}
-                  <p clas>{item.calisma_Sekli}</p>
-                </p>
-                <div className="dropdown-divider"></div>
-                <CCardBody>
-                  <CCardTitle>{item.gereksinim}</CCardTitle>
-                  <CCardText>
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </CCardText>
-                  <CButton className="align-items-end" href="#">Go somewhere</CButton>
+            {item.reverse().map((item) => (
+              <CCard
+                className="shadow-lg"
+                key={item.id}
+                style={{ margin: "2% 0" }}
+              >
+                <CCardBody className="flex">
+                  <div className="flex-career gap-2">
+                    <div className="flex justify-content-center align-items-center ratio-1x1 rounded-circle">
+                      <img
+                        className="rounded-circle"
+                        src={BASE_IMG + item.resim_Dizin}
+                        alt=""
+                        style={{ width: 45, height: 45, objectFit: "cover" }}
+                      />
+                    </div>
+                    <div className="flex-career-1">
+                      <p className="text-bold ">
+                        <strong style={{ fontSize: 16 }}>
+                          {item.pozisyon}
+                        </strong>
+                        <span className="flex-career-1">
+                          <p title={item.firma_Adi} style={{ fontSize: 12 }}>
+                            {item.firma_Adi.length > 28
+                              ? item.firma_Adi.substring(0, 28) + ".."
+                              : item.firma_Adi}
+                          </p>
+                        </span>
+                      </p>
+                    </div>
+                    <div className="flex-career-1 p-3"></div>
+
+                    <div className="flex-career-1 p-3">
+                      <div
+                        className="border rounded-2 text-center text-white"
+                        style={{ backgroundColor: "#1199B7" }}
+                      >
+                        <p className="rounded-2">
+                          İlan Tarihi : {dateFormat(item.ilan_Tarih)}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex-career-1 p-3 text-center">
+                      <p>{item.calisma_Sekli}</p>
+                    </div>
+                    <div className="flex-career-1 p-2">
+                      <button
+                        type="button "
+                        className="btn btn-outline-primary rounded-2 border-5"
+                      >
+                        İlan Detayı
+                      </button>
+                    </div>
+                  </div>
                 </CCardBody>
               </CCard>
             ))}
@@ -58,26 +99,3 @@ const Career = () => {
   )
 }
 export default Career
-
-// const Career = () => {
-//   document.querySelectorAll(".k-item .k-title").forEach((item) => {
-//     item.addEventListener("click", (event) => {
-//       let body = item.parentElement.querySelector(".k-body")
-//       if (body.classList.contains("show")) body.classList.remove("show")
-//       else body.classList.add("show")
-//     })
-//   })
-
-//   return (
-//     <section
-//       style={{
-//         margin: "10% 0"
-//       }}
-//     >
-//       <p className="fw-bolder text-center pb-5 h-auto">
-//         Şu anda aktif ilan bulunmamaktadır.
-//       </p>
-//     </section>
-//   )
-// }
-// export default Career
